2a13ffd9ec90e1cf360006670c9d4314a01538f2
TestUser <testuser@test.com>
2026-02-06 15:37:59 +0100

    Update greeting message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is Automated?'
+    return 'Hello, World! This is now Automated!'
 
 if __name__ == '__main__':
     app.run(debug=True)
